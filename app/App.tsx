f5dff1fa533ee32471a622e20e064d0ab337f7d0
princekumarpaswan <prasantandal123@gmail.com>
2024-04-04 13:16:42 +0530

    commanted     // emitDeclarationOnly: true,  causing a error

---
+++ app/App.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import { View, Text } from "react-native";
+
+const App = () => (
+  <View style={{ flex: 1, backgroundColor: "blue" }}>
+    <Text>Prince</Text>
+  </View>
+);
+
+export default App;
